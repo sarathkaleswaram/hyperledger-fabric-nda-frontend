da21b350262a716121410f1b082d94c8bd663a28
sarathkaleswaram <ksb994@gmail.com>
2019-09-13 16:57:16 +0530

    spacing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,13 @@ import { AuthGuard } from './services/auth-guard.service';
 import { AgreementPrintComponent } from './agreement-print/agreement-print.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'login', component: LoginComponent},
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'home', canActivate: [AuthGuard], component: HomeComponent },
-  { path: 'agreement', canActivate: [AuthGuard], component: AgreementComponent},
-  { path: 'agreement-print', canActivate: [AuthGuard], component: AgreementPrintComponent},
-  { path: 'transactions', canActivate: [AuthGuard], component: TransactionsComponent},
-  { path: 'add-nda', canActivate: [AuthGuard], component: AddNDAComponent}
+  { path: 'agreement', canActivate: [AuthGuard], component: AgreementComponent },
+  { path: 'agreement-print', canActivate: [AuthGuard], component: AgreementPrintComponent },
+  { path: 'transactions', canActivate: [AuthGuard], component: TransactionsComponent },
+  { path: 'add-nda', canActivate: [AuthGuard], component: AddNDAComponent }
 ];
 
 @NgModule({
